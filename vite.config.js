ced9a5eb69c3ca6ee3de8fec946f67ed0f55a6b7
Trong Linh <linhpksw@gmail.com>
2023-02-19 20:34:52 +0700

    set up tailwind

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                // main: resolve(__dirname, 'index.html'),
+                main: resolve(__dirname, 'index.html'),
+                home: resolve(__dirname, './src/templates/home-page.html'),
+            },
+        },
+    },
+});
