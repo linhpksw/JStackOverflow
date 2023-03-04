362a605e064f6abd6ab3090db6f54fd6762f48e9
DucNguyen2004 <manhduc112004@gmail.com>
2023-03-04 12:38:19 +0700

    update ver2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,6 +14,149 @@ bgcolor.addEventListener("click", function () {
   const randomColor = colors[Math.floor(Math.random() * colors.length)];
   document.querySelector(".img-user").style.backgroundColor = randomColor;
 });
+let list = document.getElementById("list");
+const search = document.getElementById("search-text");
+
+let allAnswers = [];
+let allQuestions = [];
+
+try {
+  getDataFakeAPI();
+  async function getDataFakeAPI() {
+    const responseAPI = await fetch(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const data = await responseAPI.json();
+    list.innerHTML = "Loading...";
+    setTimeout(() => {
+      list.innerHTML = "";
+
+      data.forEach((e) => {
+        // const answersItem = document.createElement("div");
+        // const questionsItem = document.createElement("div");
+
+        const divItem = document.createElement("div");
+        divItem.innerHTML = `
+        <div class="mb-2 answers-list">
+                                    <div class="pe-3 ps-3 pt-3 d-flex">
+                                        <div class="me-3">
+                                            <span>31</span>
+                                            <span>votes</span>
+                                        </div>
+                                        <div class="post-summary-stats">
+                                            <svg aria-hidden="true" class="svg-icon iconCheckmarkSm" width="14"
+                                                height="14" viewBox="0 0 14 14">
+                                                <path d=="M13 3.41 11.59 2 5 8.59 2.41 6 1 7.41l4 4 8-8Z"></path>
+                                            </svg>
+                                            Accept
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="pe-3 ps-3 pb-3">
+                                            <h5 class="title-ans">${e.title}</h5>
+                                            <div class="d-flex">
+                                                <div class="post-summary-tags d-flex">
+                                                    <ul class = "ps-0">
+                                                        <li class = "pe-1 ps-1 me-1 ms-1">#MAE101</li>
+                                                        <li class = "pe-1 ps-1 me-1 ms-1">#PRF192</li>
+                                                    </ul>
+                                                </div>
+                                                <div class="d-flex align-items-center justify-content-end flex-grow-1">
+                                                    <div>
+                                                        answer at 00:02
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+        list.appendChild(divItem);
+
+        // const questionItem = `;
+
+        //     answersItem.innerHTML = `
+        // <div class="mb-2 answers-list">
+        //                             <div class="pe-3 ps-3 pt-3 d-flex">
+        //                                 <div class="me-3">
+        //                                     <span>31</span>
+        //                                     <span>votes</span>
+        //                                 </div>
+        //                                 <div class="post-summary-stats">
+        //                                     <svg aria-hidden="true" class="svg-icon iconCheckmarkSm" width="14"
+        //                                         height="14" viewBox="0 0 14 14">
+        //                                         <path d=="M13 3.41 11.59 2 5 8.59 2.41 6 1 7.41l4 4 8-8Z"></path>
+        //                                     </svg>
+        //                                     Accept
+        //                                 </div>
+        //                             </div>
+        //                             <div>
+        //                                 <div class="pe-3 ps-3 pb-3">
+        //                                     <h5 class="title-ans">${data.title}</h5>
+        //                                     <div class="d-flex">
+        //                                         <div class="post-summary-tags d-flex">
+        //                                             <ul class = "ps-0">
+        //                                                 <li class = "pe-1 ps-1 me-1 ms-1">#MAE101</li>
+        //                                                 <li class = "pe-1 ps-1 me-1 ms-1">#PRF192</li>
+        //                                             </ul>
+        //                                         </div>
+        //                                         <div class="d-flex align-items-center justify-content-end flex-grow-1">
+        //                                             <div>
+        //                                                 answer at 00:02
+        //                                             </div>
+        //                                         </div>
+        //                                     </div>
+        //                                 </div>
+        //                             </div>
+        //                         </div>`;
+
+        //     questionsItem.innerHTML = `
+        //     <div class="my-2 answers-list">
+        //                                 <div class="pe-3 ps-3 pt-3 d-flex">
+        //                                     <div class="me-3">
+        //                                         <span>31</span>
+        //                                         <span>votes</span>
+        //                                     </div>
+        //                                     <div class="post-summary-stats">
+        //                                         <svg aria-hidden="true" class="svg-icon iconCheckmarkSm" width="14"
+        //                                             height="14" viewBox="0 0 14 14">
+        //                                             <path d="M13 3.41 11.59 2 5 8.59 2.41 6 1 7.41l4 4 8-8Z"></path>
+        //                                         </svg>
+        //                                         "Accept"
+        //                                     </div>
+        //                                     <div class="ms-3">
+        //                                         <span>1k</span>
+        //                                         <span>views</span>
+        //                                     </div>
+        //                                 </div>
+        //                                 <div>
+        //                                     <div class="pe-3 ps-3 pb-3">
+        //                                         <h5 class="title-ans">HELLO</h5>
+        //                                         <div class="d-flex">
+        //                                             <div class="post-summary-tags d-flex">
+        //                                                 <ul class="ps-0">
+        //                                                     <li>#MAE101</li>
+        //                                                     <li>#PRF192</li>
+        //                                                 </ul>
+        //                                             </div>
+        //                                             <div class="d-flex align-items-center justify-content-end flex-grow-1">
+        //                                                 <div>
+        //                                                     answer at 00:02
+        //                                                 </div>
+        //                                             </div>
+        //                                         </div>
+        //                                     </div>
+        //                                 </div>
+        //                             </div>
+        //     `;
+        // list.appendChild(answersItem);
+        // list.appendChild(questionsItem);
+      });
+    }, 1000);
+    // list.innerHTML = allAnswers.join(" ");
+  }
+} catch (err) {
+  console.log(err);
+}
 
 // const selected = document.querySelectorAll(".update-list");
 // selected.addEventListener("click", function(){
@@ -22,29 +165,3 @@ bgcolor.addEventListener("click", function () {
 //         selected[i].style.backgroundColor = randomColor;
 //     }
 // });
-
-// 1. new Promise
-// 2. executor
-var promise = new Promise(
-  function(resolve, reject){
-    // logic
-    //Thành công: resolve();
-    //Thất bại: reject();
-    resolve([
-      {
-        id: 1,
-        name: 'Javascript'
-      }
-    ]);
-});
-
-promise
-  .then(function(source){
-    console.log(source);
-  })
-  .catch(function(){
-    console.log('Failure!');
-  })
-  .finally(function(){
-    console.log('Done!');
-  });
