43cf2e5b804983dcad167b48aa8b5c18bc20e03e
Trong Linh <linhpksw@gmail.com>
2023-03-06 01:15:00 +0700

    fix sign in

--- library/static/js/sign-in.js
+++ library/static/js/sign-in.js
@@ -1,36 +1,36 @@
-try {
-    const email = document.getElementById('email');
+document.addEventListener('DOMContentLoaded', () => {
+    try {
+        const email = document.getElementById('email');
 
-    const password = document.getElementById('password');
+        const password = document.getElementById('password');
 
-    const signInBtn = document.getElementById('sign-in-btn');
+        const signInBtn = document.getElementById('sign-in-btn');
 
-    const fetchUserCredential = async () => {
-        const data = {
-            email: email.value,
-            password: password.value,
+        const fetchUserCredential = async () => {
+            const data = {
+                email: `${email.value}`,
+                password: `${password.value}`,
+            };
+
+            const opt = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            };
+
+            const URL = 'https://jstackoverflow.jsclub.me/login';
+
+            const response = await fetch(URL, opt);
+
+            const jsonResponse = await response.json();
+
+            console.log(jsonResponse);
         };
 
-        const opt = {
-            method: 'POST',
-
-            headers: {
-                'Content-Type': 'application/json',
-            },
-
-            body: JSON.stringify(data),
-        };
-
-        const URL = 'https://jstackoverflow.jsclub.me/login';
-
-        const response = await fetch(URL, opt);
-
-        const jsonResponse = await response.json();
-
-        console.log(jsonResponse);
-    };
-
-    signInBtn.addEventListener('click', fetchUserCredential);
-} catch (err) {
-    console.log(err);
-}
+        signInBtn.addEventListener('click', fetchUserCredential);
+    } catch (err) {
+        console.log(err);
+    }
+});
