1d298007def175a2a4e18bf5a2c363192c018368
AnhDat0103 <124783881+AnhDat0103@users.noreply.github.com>
2023-03-06 20:25:21 +0700

    update to get questions by asker_id

--- library/questions_app/controller.py
+++ library/questions_app/controller.py
@@ -2,7 +2,7 @@ from flask_login import current_user, login_required
 from flask import Blueprint, redirect, render_template, request, url_for
 from library.extensions import db
 from library.model.models import User, Question, Answer
-from .services import add_question_services, update_question_services, delete_question_services, get_question_services, get_all_questions_services,vote_question
+from .services import add_question_services, update_question_services, delete_question_services, get_question_services, get_all_questions_services,vote_question,get_question_by_asker_id_services
 from .services import add_answer_services, update_answer_services, delete_answer_services, get_answer_services, get_all_answers_services, get_answer_by_question_id_services
 from library.library_ma import QuestionSchema, AnswerSchema
 from flask import jsonify
@@ -21,8 +21,10 @@ main = Blueprint('main', __name__)
 
 #      }
 #     return render_template('home.html', **context)
-
-
+# @main.route('/question-page/<int:id>',methods=['GET'])
+# def question_page(id):
+#     return render_template('question_page.html')
+       
 @main.route('/questions_manager/add_question', methods=['POST'])
 @login_required
 def add_questions():
@@ -93,3 +95,8 @@ def delete_answer(id):
 @login_required
 def get_answer_by_question_id(id):
     return get_answer_by_question_id_services(id)
+
+@main.route('/questions_manager/users/<int:id>/question', methods=['GET'])
+def get_question_by_asker_id(id):
+    return get_question_by_asker_id_services()
+     
