4587df840f7116cad1ffea4dec646fc5c98c8eb6
Trong Linh <linhpksw@gmail.com>
2023-03-06 18:21:11 +0700

    add append question

--- library/static/js/home-page.js
+++ library/static/js/home-page.js
@@ -12,8 +12,104 @@ discardBtn.addEventListener('click', () => {
     modalQuestion.classList.remove('modal-open');
 });
 
-function appendQuestion() {
+function appendQuestion(id) {
     const questionsElement = document.getElementById('questions');
+
+    const totalVotes = 25;
+    const totalAnswers = 10;
+    const totalViews = 13;
+
+    const questionTitle = `ABC`;
+    const questionTag = `PRF192`;
+
+    const questionTime = `32p`;
+    const userName = 'Le Trong Linh';
+
+    const newQuestionElement = `
+    <!-- Question ${id} -->
+    <div id="question-${id}" class="flex h-auto gap-5 rounded-2xl bg-[#262D34] py-5 px-5">
+    <!-- Stats -->
+    <div id="stats" class="flex flex-none flex-col gap-2 text-white">
+        <!-- Total votes -->
+        <span id="total-votes" class="flex justify-end text-sm font-medium"
+            >${totalVotes} votes</span
+        >
+
+        <!-- Answers -->
+        <div id="answers" class="rounded-md bg-green-900 py-2 px-1.5">
+            <div class="flex items-center justify-between gap-1">
+                <!-- Correct answer -->
+                <!-- <div id="correct-answer">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="2.5"
+                        stroke="currentColor"
+                        class="h-5 w-5">
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M4.5 12.75l6 6 9-13.5" />
+                    </svg>
+                </div> -->
+
+                <!-- Total answers -->
+                <div>
+                    <span
+                        id="total-answers"
+                        class="flex justify-end text-sm font-medium"
+                        >${totalAnswers} answers</span
+                    >
+                </div>
+            </div>
+        </div>
+
+        <span id="total-views" class="flex justify-end text-sm font-medium"
+            >${totalViews} views</span
+        >
+    </div>
+
+    <!-- Infor -->
+    <div id="infors" class="flex flex-1 flex-col gap-3">
+        <!-- Question title -->
+        <div class="flex justify-between">
+            <span
+                id="question-title"
+                class="text-xl font-semibold text-amber-400 hover:text-amber-500"
+                >${questionTitle}</span
+            >
+        </div>
+
+        <!-- Tag and ask_id -->
+        <div class="flex justify-between gap-3">
+            <button
+                id="question-tag"
+                class="rounded-3xl bg-[#2C353D] py-1 px-3 text-xs font-semibold text-[#C5D0E6]">
+                $${questionTag}
+            </button>
+
+            <div class="mt-3 flex items-center justify-end">
+                <div
+                    class="mr-3 flex h-5 w-5 items-center justify-center rounded-full bg-[#F0F1FE]"></div>
+
+                <div class="flex-none">
+                    <span
+                        id="question-time"
+                        class="text-sm font-semibold text-[#C5D0E6]"
+                        ><a id="user-name" href="#" class="text-amber-400"
+                            >${userName}</a
+                        >
+                        ${questionTime}</span
+                    >
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    `;
+
+    questionsElement.insertBefore(newQuestionElement, questionsElement.firstChild);
 }
 
 //  Initialize Quill editor
@@ -62,7 +158,8 @@ const postQuestion = async () => {
         const jsonResponse = await response.json();
 
         if (jsonResponse.status == '') {
-            alert();
+            const id = 2;
+            appendQuestion(id);
             // modalQuestion.classList.remove('modal-open');
         } else {
         }
