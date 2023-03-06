03fb589592957bf64125b78c5c7b0175496bce7d
Nguyễn Gia Bình <binhnghe187025@fpt.edu.vn>
2023-03-06 20:52:13 +0700

    return question id

--- library/questions_app/services.py
+++ library/questions_app/services.py
@@ -38,7 +38,8 @@ def add_question_services():
                         'tag': new_question.tag,
                         'datetime_posted': new_question.datetime_posted,
                         'datetime_updated': new_question.datetime_updated,
-                        'asker_id': new_question.asker_id})
+                        'asker_id': new_question.asker_id,
+                        'id': new_question.id})
     except IndentationError:
         db.session.rollback()
         return jsonify({'status': "Can't add new question"})
