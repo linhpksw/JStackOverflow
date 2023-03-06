284ef211921a1ff58620b0be01179f25900470c1
DucNguyen2004 <manhduc112004@gmail.com>
2023-03-06 22:56:18 +0700

    test

--- library/static/assets/js/profile.js
+++ library/static/assets/js/profile.js
@@ -15,7 +15,7 @@ searchBar.addEventListener("keyup", (e) => {
 const getDataFakeAPI = async () => {
   try {
     const responseAPI = await fetch(
-      "https://jsonplaceholder.typicode.com/users"
+      "https://jstackoverflow.jsclub.me/user/{{id}}/questions"
     );
     data = await responseAPI.json();
     let userIds = data.filter(function (user) {
