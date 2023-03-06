52fadbdfa45ca0ebd521dcfc155007e66a4c4860
Nguyễn Gia Bình <binhnghe187025@fpt.edu.vn>
2023-03-06 22:12:20 +0700

    edit library_ma

--- library/questions_app/controller.py
+++ library/questions_app/controller.py
@@ -3,7 +3,7 @@ from flask import Blueprint, redirect, render_template,url_for
 from library.extensions import db
 from library.model.models import User, Question, Answer
 from .services import add_question_services, update_question_services, delete_question_services, get_question_services, get_all_questions_services,vote_question_services,vote_answer_services
-from .services import add_question_services, update_question_services, delete_question_services, get_question_services, get_all_questions_services,get_question_by_asker_id_services
+from .services import add_question_services, update_question_services, delete_question_services, get_question_services, get_all_questions_services
 from .services import add_answer_services, update_answer_services, delete_answer_services, get_answer_services, get_all_answers_services, get_answer_by_question_id_services
 from library.library_ma import QuestionSchema, AnswerSchema
 from flask import jsonify
@@ -89,7 +89,7 @@ def vote_answer(id):
     return vote_answer_services(id)
 
 
-@main.route('/questions_manager/users/<int:id>/question', methods=['GET'])
-def get_question_by_asker_id(id):
-    return get_question_by_asker_id_services()
+# @main.route('/questions_manager/users/<int:id>/question', methods=['GET'])
+# def get_question_by_asker_id(id):
+#     return get_question_by_asker_id_services()
      
