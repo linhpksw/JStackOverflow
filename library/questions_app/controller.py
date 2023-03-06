46567f49a2c2e055ac6bc268639bf5a01195952a
AnhDat0103 <124783881+AnhDat0103@users.noreply.github.com>
2023-03-07 00:23:46 +0700

    update return question-page.html

--- library/questions_app/controller.py
+++ library/questions_app/controller.py
@@ -13,7 +13,7 @@ main = Blueprint('main', __name__)
 # setup the questions services
 @main.route('/questions_manager/questions/<int:id>/question',methods=['GET'])
 def questions_page(id):
-        return render_template('questions-page.html',id =id) 
+        return render_template('question-page.html',id =id) 
 
       
 @main.route('/questions_manager/add_question', methods=['POST'])
