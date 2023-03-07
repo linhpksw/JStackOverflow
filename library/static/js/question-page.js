66b4c97bd52988c2667fe260e6129d437f29658d
DucNguyen2004 <manhduc112004@gmail.com>
2023-03-07 17:33:16 +0700

    test

--- library/static/js/question-page.js
+++ library/static/js/question-page.js
@@ -61,9 +61,9 @@ const question = document.getElementById("just-question");
 getQuestionFromAPI();
 
 try {
-  async function getQuestionFromAPI() {
+  async function renderQuestion() {
     const responseAPT = await fetch(
-      "https://jsonplaceholder.typicode.com/posts"
+      "https://jstackoverflow.jsclub.me/questions_manager/questions/$ { id }"
     );
     const data = await responseAPT.json();
     const divItem = document.createElement("div");
@@ -84,14 +84,12 @@ try {
         <div id="total-count"   class="text-white text-center align-middle"></div> 
         <button id="decrement-count" class="material-icons  text-white text-4xl">arrow_drop_down</button>
     </div> 
-    <p id="content-ques" class="content-question md-48 text-white float-right text-justify"> ${data.body} </p>
+    <p id="content-ques" class="content-question md-48 text-white float-right text-justify"> ${data.content} </p>
 </div>
 
 <div id="tags" class="">
     <ul class=" text-xs  ">
-        <li class="inline-block rounded-sm ">TRS601</li>
-        <li class="inline-block rounded-sm">MAE101</li>
-        <li class="inline-block rounded-sm">TRS501</li>
+        <li class="inline-block rounded-sm ">${data.tag}</li>
     </ul>
 
 </div>
@@ -109,38 +107,38 @@ const list = document.getElementById("list");
 
 getAnswerFromAPI();
 
-async function getAnswerFromAPI() {
-  const responseAPI = await fetch(
-    "https://jsonplaceholder.typicode.com/comments"
-  );
-  const data = await responseAPI.json();
-  data.forEach((data) => {
-    const divItem = document.createElement("div");
-    divItem.innerHTML = `
-    <div class="flex p-4 items-start space-x-4">
-      <img class="w-10 h-10 rounded-full" src="/src/img/meo.jpg" alt="Avatar">
-      <div id="answer1" class="m-4">
-        <div class="flex items-center justify-between">
-          <h2 class="text-lg font-medium text-orange-300"> ${data.name} </h2>
-          <p class="text-sm text-gray-400">3h ago</p>
-        </div>
-        <p class="mt-2 text-white">${data.body} </p>
-          <div id="icon-comment" class="flex  text-gray-400 mt-3">
-          <button id="count-like" class="material-icons  text-gray-400 mr-3 hover:text-orange-600 ">thumb_up </button>
-          <div id="total-like" class="align-middle text-center mr-2">4</div>
-          <button id="count-dislike" class="material-icons  text-gray-400 mr-3 hover:text-orange-600 ">thumb_down </button>
-          <div class="hover:bg-orange-600 hover:text-white px-4 font-bold text-gray-400 rounded flex items-center align-middle ">
-              <i class="material-icons inline-block ">reply </i>
-              <div class="inline-block">reply</div>
-          </div>                                
-          </div>
-      </div>
+// async function getAnswerFromAPI() {
+//   const responseAPI = await fetch(
+//     "https://jsonplaceholder.typicode.com/comments"
+//   );
+//   const data = await responseAPI.json();
+//   data.forEach((data) => {
+//     const divItem = document.createElement("div");
+//     divItem.innerHTML = `
+//     <div class="flex p-4 items-start space-x-4">
+//       <img class="w-10 h-10 rounded-full" src="/src/img/meo.jpg" alt="Avatar">
+//       <div id="answer1" class="m-4">
+//         <div class="flex items-center justify-between">
+//           <h2 class="text-lg font-medium text-orange-300"> ${data.name} </h2>
+//           <p class="text-sm text-gray-400">3h ago</p>
+//         </div>
+//         <p class="mt-2 text-white">${data.body} </p>
+//           <div id="icon-comment" class="flex  text-gray-400 mt-3">
+//           <button id="count-like" class="material-icons  text-gray-400 mr-3 hover:text-orange-600 ">thumb_up </button>
+//           <div id="total-like" class="align-middle text-center mr-2">4</div>
+//           <button id="count-dislike" class="material-icons  text-gray-400 mr-3 hover:text-orange-600 ">thumb_down </button>
+//           <div class="hover:bg-orange-600 hover:text-white px-4 font-bold text-gray-400 rounded flex items-center align-middle ">
+//               <i class="material-icons inline-block ">reply </i>
+//               <div class="inline-block">reply</div>
+//           </div>
+//           </div>
+//       </div>
 
-      </div>
-    `;
-    list.appendChild(divItem);
-  });
-}
+//       </div>
+//     `;
+//     list.appendChild(divItem);
+//   });
+// }
 
 //add question
 
